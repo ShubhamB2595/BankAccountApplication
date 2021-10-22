c6063b8156403e1cee1bf3d8bd346aaabfaec662
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-22 07:51:29 +0530

    Welcome to bank account application program

---
+++ src/application/banking/BankAccount.java
@@ -0,0 +1,11 @@
+package application.banking;
+
+public class BankAccount {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Bank account Application program");
+
+	}
+
+}
